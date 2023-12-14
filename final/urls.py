e62e17a2f72a973dc105788c59e2e5a772186572
aruuuuuuke <askatovaaruuke657@gmail.com>
2023-12-14 14:36:14 +0600

    authorisation

--- final/urls.py
+++ final/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from translater.views import LessonAPView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/lessonlist/', LessonAPView.as_view())
+    path('api/v1/lessonlist/', LessonAPView.as_view()),
+    path('api/v2/auth/', include('users.urls')),
 ]
